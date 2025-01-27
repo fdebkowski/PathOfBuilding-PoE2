03d8262e5d918a5e0cf06e423278bf4869b80b23
LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>
2025-01-27 12:39:59 +1100

    Add Shift + Ctrl + V to bypass item import confirmation box (#624)
    Adds the keybind to quickly import items into PoB without needing to confirm each time. You can also press enter now to confirm selection
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Classes/ItemsTab.lua
+++ src/Classes/ItemsTab.lua
@@ -1069,6 +1069,9 @@ function ItemsTabClass:Draw(viewPort, inputEvents)
 					end
 					self:CreateDisplayItemFromRaw(newItem, true)
 				end
+				if self.displayItem and IsKeyDown("SHIFT") then
+					self:AddDisplayItem()
+				end
 			elseif event.key == "e" then
 				local mOverControl = self:GetMouseOverControl()
 				if mOverControl and mOverControl._className == "ItemSlotControl" and mOverControl.selItemId ~= 0 then
@@ -1094,6 +1097,8 @@ function ItemsTabClass:Draw(viewPort, inputEvents)
 			elseif event.key == "d" and IsKeyDown("CTRL") then
 				self.showStatDifferences = not self.showStatDifferences
 				self.build.buildFlag = true
+			elseif self.displayItem and IsKeyDown("RETURN") then
+				self:AddDisplayItem()
 			end
 		end
 	end
