07b4083c6ae3de4c4e199401c9d6bc2e9934ab46
Wires77 <Wires77@users.noreply.github.com>
2025-08-29 00:21:11 -0500

    Fix globalLimit being sensitive to tag order (#1225)
    * Fix globalLimit being sensitive to tag order (#8652)
    
    * FEAT(items): Add showcased uniques to New.lua
    
    * FEAT(mods): add handling for new mods on Scornflux
    
    * FEAT(mods): add handling for "Take X Fire Damage when you use a Skill"
    
    * FEAT(mods): add handling for Damage penetrates "Fire Resistance equal to
    your overcapped Fire Resistance"
    
    * FEAT(mods): add handling for "Warcries have an additional Life Cost equal to 15% of your Maximum Life"
    
    * FEAT(mods): add handling for "Warcry Skills have X increased Area of
    Effect"
    
    * FEAT(mods): add handling for `Gain no armour from equipped body armour`
    
    * FEAT(mods): port generalized "doubled" mod handling from POB2
    
    https://github.com/PathOfBuildingCommunity/PathOfBuilding-PoE2/pull/1095
    
    * Update spelling and ModCache
    
    * FIX(mods): globalLimit style mods did not work sometimes
    
    When a mod gots pice wise parsed the tags can be added to the mod table
    in unpredictable order. The inital implementation was simply checking
    the first tag which caused the global limit functionality to not work
    sometimes. This commit moves the logic into EvalMod.
    
    * FEAT(mods): add support for "you have no Str/Dex" mods
    
    * FIX(spelling): un-rake-able
    
    ---------
    
    Co-authored-by: Wires77 <Wires77@users.noreply.github.com>
    
    * [PATCH 1/3] FIX: globalLimit not applying
    
    * [PATCH 2/3] MISC(test): add some more tests for globlaLimit type mods
    
    * [PATCH 3/3] MISC(test): test PerStat code path
    
    * Fix tests for PoE2
    
    ---------
    
    Co-authored-by: Paliak <91493239+Paliak@users.noreply.github.com>

--- spec/System/TestItemMods_spec.lua
+++ spec/System/TestItemMods_spec.lua
@@ -176,4 +176,48 @@ describe("TetsItemMods", function()
 
 		assert.are_not.equals(baseLife, build.calcsTab.mainOutput.Life)
 	end)
+
+	it("globalLimit mods", function()
+		build.configTab.input.customMods = [[
+			-1000% to cold resistance
+		]]
+		build.configTab:BuildModList()
+		build.itemsTab:CreateDisplayItemFromRaw([[Replica Nebulis
+			Clasped Sceptre
+			League: Heist
+			Quality: 20
+			Sockets: B-B-B
+			LevelReq: 68
+			Implicits: 1
+			40% increased Elemental Damage
+			{fractured}{range:1}(15-20)% increased Cast Speed
+			{range:1}(15-20)% increased Cold Damage per 1% Missing Cold Resistance, up to a maximum of 300%
+			{range:1}(15-20)% increased Fire Damage per 1% Missing Fire Resistance, up to a maximum of 300%]])
+		build.itemsTab:AddDisplayItem()
+		build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nFireball 20/0 Default  1\n")
+		runCallback("OnFrame")
+
+		assert.are_not.equals(340, build.calcsTab.mainEnv.modDB:Sum("INC", "FireDamage"))
+		assert.are_not.equals(340, build.calcsTab.mainEnv.modDB:Sum("INC", "ColdDamage"))
+
+		newBuild()
+
+		build.configTab.input.customMods = [[
+			Gain 25% increased Armour per 5 Power for 8 seconds when you Warcry, up to a maximum of 100%
+			Warcries have infinite Power
+			warcries grant arcane surge to you and allies, with 10% increased effect per 5 power, up to 100%
+		]]
+		build.configTab:BuildModList()
+		build.itemsTab:CreateDisplayItemFromRaw([[
+			New Item
+			Fur Plate
+			Armour: 60
+		]])
+		build.itemsTab:AddDisplayItem()
+		build.skillsTab:PasteSocketGroup("Arc 20/0 Default  1")
+
+		assert.are_not.equals(20, build.calcsTab.mainEnv.modDB:Sum("MORE", { flags = ModFlag.Cast }, "Speed"))
+		assert.are_not.equals(120, build.calcsTab.mainOutput.Armour)
+		runCallback("OnFrame")
+	end)
 end)
--- src/Classes/ModStore.lua
+++ src/Classes/ModStore.lua
@@ -283,12 +283,13 @@ function ModStoreClass:GetStat(stat, cfg)
 	end
 end
 
-function ModStoreClass:EvalMod(mod, cfg)
+function ModStoreClass:EvalMod(mod, cfg, globalLimits)
 	local value = mod.value
 	for _, tag in ipairs(mod) do
 		if tag.type == "Multiplier" then
 			local target = self
 			local limitTarget = self
+
 			-- Allow limiting a self multiplier on a parent multiplier (eg. Agony Crawler on player virulence)
 			-- This explicit target is necessary because even though the GetMultiplier method does call self.parent.GetMultiplier, it does so with noMod = true,
 			-- disabling the summation (3rd part): (not noMod and self:Sum("BASE", cfg, multiplierName[var]) or 0)
@@ -825,6 +826,18 @@ function ModStoreClass:EvalMod(mod, cfg)
 				return
 			end
 		end
-	end	
+	end
+
+	-- Apply global limits
+	for _, tag in ipairs(mod) do
+		if globalLimits and tag.globalLimit and tag.globalLimitKey then
+			value = value or 0
+			globalLimits[tag.globalLimitKey] = globalLimits[tag.globalLimitKey] or 0
+			if globalLimits[tag.globalLimitKey] + value > tag.globalLimit then
+				value = tag.globalLimit - globalLimits[tag.globalLimitKey]
+			end
+			globalLimits[tag.globalLimitKey] = globalLimits[tag.globalLimitKey] + value
+		end
+	end
 	return value
 end
